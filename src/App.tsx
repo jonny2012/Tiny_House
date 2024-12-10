0512f97ef167fd9b87e418e3ddaba19fe86c9d98
Jonny <vanea.ss21@gmail.com>
2024-12-10 22:27:50 +0100

    initial config

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import { useState } from "react";
+import "./App.module.scss";
+import React from "react";
+import styles from "./App.module.scss";
+
+function App() {
+  return <h1 className={styles.main}>Hello</h1>;
+}
+
+export default App;
